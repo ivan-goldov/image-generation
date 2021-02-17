024d5049ad9216cdebae49c22027eb2907a3cd1b
Ivan Goldov <ivangoldov@gmail.com>
2021-02-17 19:55:00 +0300

    add bash script to download model

---
+++ bin/download_pretrained_model_horse2zebra.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# Linux
+wget=/usr/bin/wget
+
+wget -q -O "D_A" "https://www.dropbox.com/s/1c6rymgbmygwx8l/D_A199?dl=0"
+wget -q -O "D_B" "https://www.dropbox.com/s/llleefakk3b8ae6/D_B199?dl=0"
+wget -q -O "G_AB" "https://www.dropbox.com/s/q5mjogixqnbak2k/G_AB_epoch199?dl=0"
+wget -q -O "G_BA" "https://www.dropbox.com/s/v45px1q1sltf7n4/G_BA_epoch199?dl=0"
+echo "All models downloaded"
