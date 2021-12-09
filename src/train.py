f5afc2f2631a3990283f84384413b71492d2668a
Ivan Goldov <47887209+ivangoldov@users.noreply.github.com>
2021-12-09 15:12:56 +0300

    Update train.py

--- src/train.py
+++ src/train.py
@@ -35,12 +35,10 @@ losses_a = []
 losses_b = []
 
 
-def train(data, G_AB, G_BA, D_A, D_B, 
-          n_epochs=args.n_epochs, batch_size=args.batch_size, lr=args.lr, lmbda=args.lmbda, 
-          save_models=args.save_models, save_losses=args.save_losses, 
-          shuffle=args.shuffle, shift=args.shift):
-    global losses_gan, losses_a, losses_b, DEVICE, RESCALE_SIZE
-    losses_gan = []
+def train(data, G_AB, G_BA, D_A, D_B, n_epochs=200, batch_size=1, lr=0.0002, lmbda=10.0, save_models=True, 
+          save_losses=True, shuffle=True, shift=0, input_c=3, output_c=3):
+    global losses_adv, losses_a, losses_b, DEVICE, RESCALE_SIZE
+    losses_adv = []
     losses_a = []
     losses_b = []
 
@@ -70,18 +68,18 @@ def train(data, G_AB, G_BA, D_A, D_B,
     optimizer_B = torch.optim.Adam(params=D_B.parameters(), lr=lr, betas=(0.5, 0.999))
 
     lr_scheduler_G = torch.optim.lr_scheduler.LambdaLR(optimizer_G, 
-                                                       lr_lambda=LambdaLR(n_epochs, 0, n_epochs/2).step)
+                                                       lr_lambda=LambdaLR(n_epochs-shift, 0, n_epochs/2-shift).step)
     lr_scheduler_A = torch.optim.lr_scheduler.LambdaLR(optimizer_A, 
-                                                         lr_lambda=LambdaLR(n_epochs, 0, n_epochs/2).step)
+                                                         lr_lambda=LambdaLR(n_epochs, 0, n_epochs/2-shift).step)
     lr_scheduler_B = torch.optim.lr_scheduler.LambdaLR(optimizer_B, 
-                                                         lr_lambda=LambdaLR(n_epochs, 0, n_epochs/2).step)
+                                                         lr_lambda=LambdaLR(n_epochs, 0, n_epochs/2-shift).step)
     
 
     fake_A_buffer = Buffer()
     fake_B_buffer = Buffer()
 
-    with tqdm(desc="epoch", total=n_epochs) as pbar_outer:
-        for epoch in range(shift, n_epochs+shift):
+    with tqdm(desc="epoch", total=n_epochs-shift) as pbar_outer:
+        for epoch in range(shift+1, n_epochs+shift+1):
             lg = 0
             la = 0
             lb = 0
@@ -98,7 +96,7 @@ def train(data, G_AB, G_BA, D_A, D_B,
                 optimizer_B.zero_grad()
                 k += 1
 
-                # training generators 
+                # identity loss
                 id_B = G_AB(real_B)
                 loss1 = id_loss(id_B, real_B) * (lmbda / 2)
                 id_A = G_BA(real_A)
@@ -111,19 +109,19 @@ def train(data, G_AB, G_BA, D_A, D_B,
                 detect_fake_A = D_A(fake_A)
                 loss4 = generative_loss(detect_fake_A, target_real)
 
-                # training cycle consistancy
+                # cycle loss
                 cycle_A = G_BA(fake_B)
                 loss5 = cycle_loss(cycle_A, real_A) * lmbda
                 cycle_B = G_AB(fake_A)
                 loss6 = cycle_loss(cycle_B, real_B) * lmbda
 
-                losses_gan = loss1 + loss2 + loss3 + loss4 + loss5 + loss6
-                losses_gan.backward()
-                lg += losses_gan.item()
+                loss_adv = loss1 + loss2 + loss3 + loss4 + loss5 + loss6
+                loss_adv.backward()
+                lg += loss_adv.item()
 
                 optimizer_G.step()
 
-                # training A discriminator
+                # adversarial loss A
                 pred_real = D_A(real_A)
                 loss7 = generative_loss(pred_real, target_real)
 
@@ -131,13 +129,13 @@ def train(data, G_AB, G_BA, D_A, D_B,
                 pred_fake = D_A(fake_A.detach())
                 loss8 = generative_loss(pred_fake, target_fake)
 
-                losses_a = (loss7 + loss8) * 0.5
-                losses_a.backward()
-                la += losses_a.item()
+                loss_a = (loss7 + loss8) * 0.5
+                loss_a.backward()
+                la += loss_a.item()
 
                 optimizer_A.step()
 
-                # training B discriminator
+                # adversarial loss B
                 pred_real = D_B(real_B)
                 loss9 = generative_loss(pred_real, target_real)
 
@@ -145,9 +143,9 @@ def train(data, G_AB, G_BA, D_A, D_B,
                 pred_fake = D_B(fake_B.detach())
                 loss10 = generative_loss(pred_fake, target_fake)
 
-                losses_b = (loss9 + loss10) * 0.5
-                losses_b.backward()
-                lb += losses_b.item()
+                loss_b = (loss9 + loss10) * 0.5
+                loss_b.backward()
+                lb += loss_b.item()
                 
                 optimizer_B.step()
 
@@ -157,26 +155,18 @@ def train(data, G_AB, G_BA, D_A, D_B,
             lr_scheduler_A.step()
             lr_scheduler_B.step()
 
-            losses_gan.append(lg / k)
+            losses_adv.append(lg / k)
             losses_a.append(la / k)
             losses_b.append(lb / k)
+            wandb.log({'loss_adv_a_b': la / k,
+                       'loss_adv_b_a': lb / k,
+                       'loss_cycle_consistancy': lg / k})
+            print(epoch, losses_adv[-1], losses_a[-1], losses_b[-1], '---------', sep='\n')
+            torch.save(G_AB.state_dict(), f'{args['dataroot']}/saved_models/G_AB_{dataset}')
+            torch.save(G_BA.state_dict(), f'{args['dataroot']}/saved_models/G_BA_{dataset}')
+            torch.save(D_A.state_dict(), f'{args['dataroot']}/saved_models/D_A_{dataset}')
+            torch.save(D_B.state_dict(), f'{args['dataroot']}/saved_models/D_B_{dataset}')
 
-            if epoch % 20 == 19:
-                if save_losses:
-                    with open('/losses/lossgan_epoch{}'.format(epoch), 'wb') as f:
-                        pickle.dump(losses_gan, f)
-                    with open('/losses/losses_a_epoch{}'.format(epoch), 'wb') as f:
-                        pickle.dump(losses_a, f)
-                    with open('/losses/losses_b_epoch{}'.format(epoch), 'wb') as f:
-                        pickle.dump(losses_b, f)
-
-                if save_models:
-                    torch.save(G_AB.state_dict(), '/saved-models/G_AB_epoch{}'.format(epoch))
-                    torch.save(G_BA.state_dict(), '/saved-models/G_BA_epoch{}'.format(epoch))
-                    torch.save(D_A.state_dict(), '/saved-models/D_A{}'.format(epoch))
-                    torch.save(D_B.state_dict(), '/saved-models/D_B{}'.format(epoch))
-                if real_A.shape[1] != 1:
-                    with torch.no_grad():
-                        plot(real_A, G_AB(real_A), G_BA(real_A))
-
-
+            if real_A.shape[1] == input_c:
+                with torch.no_grad():
+                    plot(real_A, G_AB(real_A), G_BA(G_AB(real_A)))
